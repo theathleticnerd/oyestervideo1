4ead514408b0810ff528a7f6e2e43897a1ea73f5
Job Fernandez <jobandjeffy@gmail.com>
2020-12-15 23:47:35 +0530

    Initialcommit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import { VideoPlayer } from "./VideoPlayer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <VideoPlayer />
+    </div>
+  );
+}
---
+++ src/VideoPlayer.js
@@ -0,0 +1,56 @@
+import React, { useRef, useState, useEffect } from "react";
+import videojs from "video.js";
+require("videojs-hls-quality-selector");
+export const VideoPlayer = (props) => {
+  const videoPlayerRef = useRef(null); // Instead of ID
+  const [currentTime, setCurrentTime] = useState(null);
+  const videoSrc = "https://media.w3.org/2010/05/sintel/trailer_hd.mp4";
+
+  const videoJSOptions = {
+    autoplay: "muted",
+    controls: true,
+    userActions: { hotkeys: true },
+    playbackRates: [0.5, 1, 1.5, 2],
+    qualitySelector: true,
+    chromecast: {
+      appId: "APP-ID",
+      metadata: {
+        title: "Title display on tech wrapper",
+        subtitle: "Synopsis display on tech wrapper",
+      },
+    },
+  };
+
+  useEffect(() => {
+    if (videoPlayerRef) {
+      const player = videojs(videoPlayerRef.current, videoJSOptions, () => {
+        player.src(videoSrc);
+
+        player.hlsQualitySelector({
+          displayCurrentQuality: true,
+        });
+        player.on("ended", () => {
+          console.log("ended");
+        });
+        player.on("timeupdate", () => {
+          setCurrentTime(player.currentTime());
+        });
+        console.log("Player Ready");
+      });
+    }
+
+    return () => {};
+  }, []);
+
+  return (
+    <div onContextMenu={(e) => e.preventDefault()} style={{ width: "100%" }}>
+      <video
+        style={{ width: "100%" }}
+        ref={videoPlayerRef}
+        className="video-js"
+      />
+      <span>Current Time: {currentTime}</span>
+      {/* <GlobalStyle /> */}
+    </div>
+  );
+};
